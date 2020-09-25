f11ef43d3fd608ca0544c83fce8c5a6a2cb79cb6
bobha14 <bobha14@gmail.com>
2020-09-26 00:46:05 +0900

    button handler with controller

--- src/configController.java
+++ src/configController.java
@@ -1,2 +1,17 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class configController {
+    @FXML
+    private void handleNext(ActionEvent event) {
+        Stage s = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        s.setScene(new Scene(new Pane()));
+    }
 }
