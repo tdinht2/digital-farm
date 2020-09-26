5d9e57aa1293df109ff6b7156af59b5bfb7a2734
bobha14 <bobha14@gmail.com>
2020-09-26 12:17:59 +0900

    name button

--- src/configController.java
+++ src/configController.java
@@ -1,2 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class configController {
+    @FXML
+    private TextField name;
+
+    @FXML
+    private void handleNext(ActionEvent event) {
+        Stage s = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        s.setScene(new Scene(new Pane()));
+    }
+
+    @FXML
+    private void handleButon(ActionEvent event) {
+
+        System.out.println(name.getText());
+
+
+    }
 }
+
