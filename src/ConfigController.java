db12029c3ff653a75d469b16e717764144fe7303
atan70 <atan70@gatech.edu>
2020-10-03 20:36:00 -0400

    Delete old DigitalFarm class

--- src/ConfigController.java
+++ src/ConfigController.java
@@ -1,7 +1,6 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import main.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
