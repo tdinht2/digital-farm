7d5681bcb172e3f0e075ac84370dd60186161966
atan70 <atan70@gatech.edu>
2020-11-22 16:33:18 -0500

    add tests for harvest/water limits

--- src/testing/TestPlayer.java
+++ src/testing/TestPlayer.java
@@ -115,7 +115,7 @@ public class TestPlayer {
         Player player = new Player("Player", 0, 99999,
                 1, new Market(1));
         Crop nonHarvest = new Crop(1, Crop.Type.Corn);
-        Crop harvest = new Crop(3, Crop.Type.Corn);
+        Crop harvest = new Crop(7, Crop.Type.Corn);
         Assert.assertEquals(false, nonHarvest.canHarvest());
         Assert.assertEquals(true, harvest.canHarvest());
     }
@@ -140,5 +140,53 @@ public class TestPlayer {
         boolean canBuy = market.buy(player.getMoney(), crop, 1100,
                 player.getMaxInventorySpace() - player.getInventoryCount());
         Assert.assertEquals(canBuy, false);
+    }
+
+    @Test
+    public void testGetDailyHarvest() {
+        Player player = new Player("Player", 1000, 1);
+        Crop rice = new Crop(7, Crop.Type.Rice);
+        player.harvest(rice);
+        player.harvest(rice);
+        Assert.assertEquals(2, player.getDailyHarvest());
+    }
+
+    @Test
+    public void testDefaultHarvestLimit() {
+        Player player = new Player("Player", 1000, 1);
+        Crop rice = new Crop(7, Crop.Type.Rice);
+        player.harvest(rice);
+        player.harvest(rice);
+        player.harvest(rice);
+        Assert.assertFalse(player.harvest(rice));
+    }
+
+    @Test
+    public void testIncreasedHarvestLimit() {
+        Player player = new Player("Player", 1000, 1);
+        Crop rice = new Crop(7, Crop.Type.Rice);
+        player.setMaxHarvest(10);
+        for (int i = 0; i < 10; i++) {
+            player.harvest(rice);
+        }
+        Assert.assertFalse(player.harvest(rice));
+    }
+
+    @Test
+    public void testDefaultWaterLimit() {
+        Player player = new Player("Player", 1000, 1);
+        Assert.assertTrue(player.getDailyWater() < player.getMaxWater());
+        player.setDailyWater(7);
+        Assert.assertTrue(player.getDailyWater() >= player.getMaxWater());
+    }
+
+    @Test
+    public void testIncreasedWaterLimit() {
+        Player player = new Player("Player", 1000, 1);
+        player.setMaxWater(20);
+        player.setDailyWater(15);
+        Assert.assertTrue(player.getDailyWater() < player.getMaxWater());
+        player.setDailyWater(20);
+        Assert.assertTrue(player.getDailyWater() >= player.getMaxWater());
     }
 }
